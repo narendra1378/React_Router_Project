59558e2eeb355b42c6efb8a4d984d0d962a29c6d
narendra1378 <narendra000mahajan@gmail.com>
2024-08-25 06:42:45 +0530

    changes

--- src/components/Github/GIthub.jsx
+++ src/components/Github/GIthub.jsx
@@ -1,3 +1,25 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+  const data = useLoaderData();
+
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl flex items-center ">
+      <div className="m-4">Github followers: {data.followers}</div>
+      <img src={data.avatar_url} alt="GitHub Avatar" width={300} />
+    </div>
+  );
+}
+
+export default Github;
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/narendra1378");
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
+  }
+  return response.json();
+};
 // import React, { useEffect, useState } from "react";
 // import { useLoaderData } from "react-router-dom";
 
@@ -59,25 +81,3 @@
 //   const response = await fetch('"https://api.github.com/users/narendra1378"');
 //   return response.json();
 // };
-import React from "react";
-import { useLoaderData } from "react-router-dom";
-
-function Github() {
-  const data = useLoaderData();
-
-  return (
-    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl flex items-center ">
-      <div className="m-4">Github followers: {data.followers}</div>
-      <img src={data.avatar_url} alt="GitHub Avatar" width={300} />
-    </div>
-  );
-}
-
-export default Github;
-export const githubInfoLoader = async () => {
-  const response = await fetch("https://api.github.com/users/narendra1378");
-  if (!response.ok) {
-    throw new Error("Network response was not ok");
-  }
-  return response.json();
-};
